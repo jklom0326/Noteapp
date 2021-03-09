87c0602f4f1453046ad08717490cd7506745246f
Sangwan Han <jklom0326@gmail.com>
2021-03-09 23:59:48 +0900

    03-09

--- app/src/main/java/com/noteapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/noteapp/fragment/HomeFragment.kt
@@ -3,14 +3,22 @@ package com.noteapp.fragment
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.observe
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.noteapp.MainActivity
 import com.noteapp.R
+import com.noteapp.adapter.NoteAdapter
 import com.noteapp.databinding.FragmentHomeBinding
+import com.noteapp.model.Note
+import com.noteapp.viewmodel.NoteViewModel
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var noteViewModel: NoteViewModel
+    private lateinit var noteAdapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,19 +37,58 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
         return  binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        noteViewModel = (activity as MainActivity).noteViewModel
+        setUpRecyclerView()
+
+        binding.fbAddNote.setOnClickListener {mView ->
+            mView.findNavController().navigate(R.id.action_homeFragment_to_newNoteFragment)
+        }
+    }
+
+    private fun setUpRecyclerView(){
+        noteAdapter = NoteAdapter()
+
+        binding.recyclerView.apply {
+            layoutManager = StaggeredGridLayoutManager(
+                2,
+                StaggeredGridLayoutManager.VERTICAL
+            )
+
+            setHasFixedSize(true)
+            adapter = noteAdapter
+        }
+
+        activity?.let {
+            noteViewModel.getAllNote().observe(viewLifecycleOwner, { note ->
+                noteAdapter.differ.submitList(note)
+                updateUI(note)
+            })
+        }
+    }
+
+    private fun updateUI(note: List<Note>){
+
+        if (note.isNotEmpty()){
+            binding.recyclerView.visibility = View.VISIBLE
+            binding.tvNoNotesAvailable.visibility = View.GONE
+        } else {
+            binding.recyclerView.visibility = View.GONE
+            binding.tvNoNotesAvailable.visibility = View.VISIBLE
+        }
+
+    }
+
     // 메뉴만드는법
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.home_menu, menu)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        binding.fbAddNote.setOnClickListener {mView ->
-            mView.findNavController().navigate(R.id.action_homeFragment_to_newNoteFragment)
-        }
-    }
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
--- app/src/main/java/com/noteapp/fragment/NewNoteFragment.kt
+++ app/src/main/java/com/noteapp/fragment/NewNoteFragment.kt
@@ -2,21 +2,28 @@ package com.noteapp.fragment
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.noteapp.MainActivity
 import com.noteapp.R
 import com.noteapp.databinding.FragmentNewNoteBinding
+import com.noteapp.model.Note
+import com.noteapp.toast
+import com.noteapp.viewmodel.NoteViewModel
 
 class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
 
     private var _binding: FragmentNewNoteBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var noteViewModel: NoteViewModel
+    private lateinit var mView: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setHasOptionsMenu(true)
-
-
     }
 
     override fun onCreateView(
@@ -32,10 +39,50 @@ class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        noteViewModel = (activity as MainActivity).noteViewModel
+        mView = view
+    }
+
+    private fun  saveNote(view: View){
+
+        val noteTitle = binding.etNoteTitle.text.toString().trim()
+        val noteBody = binding.etNoteBody.text.toString().trim()
+
+        if (noteTitle.isNotEmpty()){
+
+            val note = Note(0, noteTitle, noteBody)
+            noteViewModel.addNote(note)
+            Snackbar.make(
+                view,
+                "Note saved successfully",
+                Snackbar.LENGTH_SHORT
+            ).show()
+
+            view.findNavController().navigate(
+                R.id.action_newNoteFragment_to_homeFragment
+            )
+        }else{
+            activity?.toast("Please enter note Title!")
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.save_menu ->{
+                saveNote(mView)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         menu.clear()
-        inflater.inflate(R.menu.new_note_menu,menu)
+        inflater.inflate(R.menu.new_note_menu, menu)
     }
 
     override fun onDestroy() {
